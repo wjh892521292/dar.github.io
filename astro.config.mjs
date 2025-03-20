fa122c44c18430785b3dbd8bd0fceb31018a3508
wjh892521292 <34273322+wjh892521292@users.noreply.github.com>
2025-03-20 12:05:15 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  site: '<https://tmg0.github.io>',
   integrations: [tailwind(), icon(), mdx(), react()],
   markdown: {
     shikiConfig: {
